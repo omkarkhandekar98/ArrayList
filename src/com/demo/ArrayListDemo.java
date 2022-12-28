fa7f13b75789a6f96ccb3fc8d0792b1efd12bb01
omkarkhandekar98 <omkar.khandekar1805@gmail.com>
2022-12-28 19:44:51 +0530

    First commit:ArrayList Code

---
+++ src/com/demo/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+public static void main(String[] args) {
+	
+	ArrayList<Integer> list=new ArrayList<Integer>();
+	list.add(10);
+	list.add(20);
+	
+	System.out.println(list);
+}
+}
